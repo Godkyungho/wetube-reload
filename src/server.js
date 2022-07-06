9ce351efb2d77ad901bc0f04be71a6e38f57ca1a
Godkyungho <98026101+Godkyungho@users.noreply.github.com>
2022-07-06 22:29:31 +0900

    22.07.06_wetube

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
 //강의 2.2부터 진행
 
-import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalrouter";
 import userRouter from "./routers/userrouter";
 import videoRouter from "./routers/videorouter";
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -16,16 +13,10 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/", globalRouter);
-
 app.use("/users", userRouter);
-
 app.use("/videos", videoRouter);
 
+export default app;
+
 //아래부터 외부에 개방
-
-const handleListening = () =>
-  console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
