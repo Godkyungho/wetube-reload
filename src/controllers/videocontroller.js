bd87c75900d46627e243bb9d70c4f052e224f258
Godkyungho <98026101+Godkyungho@users.noreply.github.com>
2022-07-08 00:56:12 +0900

    2022.07.08_wetube

--- src/controllers/videocontroller.js
+++ src/controllers/videocontroller.js
@@ -1,9 +1,21 @@
 import Video from "../models/video";
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {
+/*Video.find({}, (error, videos) => {
+  if(error){
+    return res.render("server-error")
+  }
+  return res.render("home", { pageTitle: "Home", videos })
+});
+
+*/
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({}); //데이터 베이스 전송받을때까지
     return res.render("home", { pageTitle: "Home", videos });
-  });
+  } catch {
+    return res.render("server-error");
+  }
 };
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -23,8 +35,18 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   //here we will add a video to the videos array.
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  await Video.create({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
   return res.redirect("/");
 };
