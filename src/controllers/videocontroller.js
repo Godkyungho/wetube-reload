d7c0b4f3487ba9bfbbe9c99984ef7bb5b4c0505c
Godkyungho <98026101+Godkyungho@users.noreply.github.com>
2022-08-01 22:12:04 +0900

    2022.08.01_wetube

--- src/controllers/videocontroller.js
+++ src/controllers/videocontroller.js
@@ -44,11 +44,8 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
-  await video.save();
   return res.redirect(`/videos/${id}`);
 };
 
@@ -63,9 +60,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
@@ -74,4 +69,11 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  //delete video
+  return res.redirect("/");
 };
